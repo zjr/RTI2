0c064f0eba6ec692e45c32de36ae54dc0ed02fce
zjr <me@zjr.io>
2013-05-11 16:14:41 -0400

    Add some currently useless callbacks to the calendar.

--- app/public/scripts/views/event-calendar-view.js
+++ app/public/scripts/views/event-calendar-view.js
@@ -5,16 +5,28 @@ define(['backbone', 'fullcal'], function (Backbone) {
 		initialize: function () {
 			this.$el.fullCalendar({
 
+				// This means I'm actually making two calls
+				// to the database for the same collection, this
+				// is of course bad I should try and avoid it.
 				events: {
 					url: '/api/events',
 					type: 'GET',
 					success: function (e) {
-						console.log('success');
+						console.log('success', e);
 					},
 					error: function () {
 						console.log('AJAX Error: GET /api/events');
 					},
+					allDayDefault: false,
 					color: '#04c'
+				},
+
+				dayClick: function () {
+					console.log(this, arguments);
+				},
+
+				eventClick: function () {
+					console.log(this, arguments);
 				}
 
 			});
